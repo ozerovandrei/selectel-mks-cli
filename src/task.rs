22100a47c707bbfe7e4c2ddc16840320af9708e8
Andrei Ozerov <andrei.ozerov92@gmail.com>
2020-08-19 16:55:01 +0300

    Implement task get and refactor output functions (#17)
    Add task get implementation.
    
    Add json mod to serialize and print types that implement
    serde::Serialize.
    
    Add common table functions into task mod.
    
    Add serde 1.0 ito dependencies.

--- src/task.rs
+++ src/task.rs
@@ -5,21 +5,23 @@ use term_table::row::Row;
 use term_table::table_cell::{Alignment, TableCell};
 use term_table::{Table, TableStyle};
 
-pub(crate) fn list(client: &Client, cluster_id: &str, output: &str) -> Result<()> {
-    let tasks = client
-        .list_tasks(cluster_id)
-        .context("Failed to list cluster tasks")?;
+use crate::json;
+
+pub(crate) fn get(client: &Client, output: &str, cluster_id: &str, task_id: &str) -> Result<()> {
+    let task = client
+        .get_task(cluster_id, task_id)
+        .context("Failed to get cluster task")?;
 
     match output {
-        "table" => list_print_table(tasks),
-        "json" => list_print_json(tasks)?,
+        "table" => get_print_table(&task),
+        "json" => json::print_json(task)?,
         _ => bail!("Unknown output format"),
     };
 
     Ok(())
 }
 
-fn list_print_table(tasks: Vec<task::schemas::Task>) {
+fn new_table_with_headers<'a>() -> Table<'a> {
     let mut table = Table::new();
     table.style = TableStyle::simple();
 
@@ -31,29 +33,52 @@ fn list_print_table(tasks: Vec<task::schemas::Task>) {
         TableCell::new_with_alignment("status", 1, Alignment::Center),
     ]));
 
-    for task in tasks.iter() {
-        let updated_at = match &task.updated_at {
-            Some(time) => time.to_rfc3339(),
-            None => String::new(),
-        };
+    table
+}
 
-        table.add_row(Row::new(vec![
-            TableCell::new(&task.id),
-            TableCell::new(&task.started_at.to_rfc3339()),
-            TableCell::new(updated_at),
-            TableCell::new(&task.task_type),
-            TableCell::new(&task.status),
-        ]));
-    }
+fn add_task_table_row(table: &mut Table, task: &task::schemas::Task) {
+    let updated_at = match &task.updated_at {
+        Some(time) => time.to_rfc3339(),
+        None => String::new(),
+    };
+
+    table.add_row(Row::new(vec![
+        TableCell::new(&task.id),
+        TableCell::new(&task.started_at.to_rfc3339()),
+        TableCell::new(updated_at),
+        TableCell::new(&task.task_type),
+        TableCell::new(&task.status),
+    ]));
+}
+
+fn get_print_table(task: &task::schemas::Task) {
+    let mut table = new_table_with_headers();
+
+    add_task_table_row(&mut table, task);
 
     println!("{}", table.render());
 }
 
-fn list_print_json(tasks: Vec<task::schemas::Task>) -> Result<()> {
-    let serialized =
-        serde_json::to_string(&tasks).map_err(selectel_mks::error::Error::SerializeError)?;
+pub(crate) fn list(client: &Client, output: &str, cluster_id: &str) -> Result<()> {
+    let tasks = client
+        .list_tasks(cluster_id)
+        .context("Failed to list cluster tasks")?;
 
-    println!("{}", serialized);
+    match output {
+        "table" => list_print_table(&tasks),
+        "json" => json::print_json(tasks)?,
+        _ => bail!("Unknown output format"),
+    };
 
     Ok(())
+}
+
+fn list_print_table(tasks: &[task::schemas::Task]) {
+    let mut table = new_table_with_headers();
+
+    for task in tasks.iter() {
+        add_task_table_row(&mut table, task);
+    }
+
+    println!("{}", table.render());
 }
