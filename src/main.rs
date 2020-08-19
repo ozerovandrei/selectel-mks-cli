e596432314c7b40228846ac8216c08d019519cc4
Andrei Ozerov <andrei.ozerov92@gmail.com>
2020-08-19 14:37:40 +0300

    Implement kubeversion list command (#14)
    Implement kubeversion list with -o table or json argument.
    
    Fix visibility in conf mod.
    
    Add anyhow, selectel-mks, serde_json, structopt, term-table
    dependencies.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,22 @@
+use anyhow::{Context, Result};
+use selectel_mks::Client;
 use structopt::StructOpt;
 
 mod conf;
+mod kubeversion;
 
-fn main() {
-    let opt = conf::CliOptions::from_args();
-    println!("{:#?}", opt);
+fn main() -> Result<()> {
+    let cli_opts = conf::CliOptions::from_args();
+
+    let client = Client::new(&cli_opts.mks_endpoint, &cli_opts.mks_token)
+        .context("Failed to initialize MKS client")?;
+
+    if let conf::Resource::Kubeversion(conf::Kubeversion {
+        command: conf::KubeversionCommand::List { output },
+    }) = cli_opts.resource
+    {
+        kubeversion::list(&client, &output)?;
+    }
+
+    Ok(())
 }
