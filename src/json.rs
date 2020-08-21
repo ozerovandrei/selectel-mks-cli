8cc82694ccaab4a7e5001997ce910943fe8b032e
Andrei Ozerov <andrei.ozerov92@gmail.com>
2020-08-21 16:51:01 +0300

    Use serde_json::to_string_pretty (#19)
    Use pretty json for outputs.

--- src/json.rs
+++ src/json.rs
@@ -3,7 +3,7 @@ use serde::Serialize;
 
 pub(crate) fn print_json<T: Serialize>(data: T) -> Result<()> {
     let serialized =
-        serde_json::to_string(&data).map_err(selectel_mks::error::Error::SerializeError)?;
+        serde_json::to_string_pretty(&data).map_err(selectel_mks::error::Error::SerializeError)?;
 
     println!("{}", serialized);
 
